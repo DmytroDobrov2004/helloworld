fbd244352e7e0aeeb57b1eb5c5fdef91275d954a
Dobrov Dmytro <dimon.dobrow@gmail.com>
2022-10-06 17:57:38 +0200

    2 commit

--- src/pl/vistula/Main.java
+++ src/pl/vistula/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System
+        System.out.println("Hello world!");
+        System.out.println("Hello woryhehld!");
     }
 }
